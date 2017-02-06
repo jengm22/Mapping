c7e6a2c3bd3460d878480a0d4000d5e26dfe6350
1jengm22 <mahtarrjeng@gmail.com>
2017-02-06 12:57:42 +0000

    android menu opening another page with buttons

--- app/src/main/java/com/example/a1jengm22/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1jengm22/mapping/HelloMap.java
@@ -2,8 +2,12 @@ package com.example.a1jengm22.mapping;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
@@ -20,6 +24,8 @@ public class HelloMap extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
         Configuration.getInstance().load
@@ -29,5 +35,26 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            // react to the menu item being selected...
+           // System.exit(0); this code closes the app
+
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/a1jengm22/mapping/MapChooseActivity.java
@@ -0,0 +1,19 @@
+package com.example.a1jengm22.mapping;
+
+/**
+ * Created by 1jengm22 on 06/02/2017.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class MapChooseActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mca);
+    }
+}
